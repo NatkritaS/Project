6bc0f02deb1f8f43d51706381774ec63fc0575bc
NatkritaS <158443165+NatkritaS@users.noreply.github.com>
2024-04-20 18:11:29 +0700

    howtoplay

--- src/Game/Howtoplay.java
+++ src/Game/Howtoplay.java
@@ -1,6 +1,10 @@
 package Game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,6 +14,7 @@ public class Howtoplay {
 	private JFrame frame;
 	private JLabel screen;
 	private JPanel panel;
+	private JButton button_back;
 
 	public Howtoplay(JFrame j) {
 		frame = j ;
@@ -25,10 +30,32 @@ public class Howtoplay {
 		screen = new JLabel();
 	    screen.setIcon(BG_Howto);
 	    screen.setBounds(0, 0, 700, 800);
-	    panel.add(screen);
-	      
-	    frame.add(panel);
+	    
+	    
+	    
+	    ImageIcon back = new ImageIcon("src\\images\\back_button.png");
+        button_back = new JButton();
+        button_back.setIcon(back);
+        button_back.setBorderPainted(false); // ทำให้กรอบตรงปุ่มหายไป
+        button_back.setContentAreaFilled(false); // ทำให้พื้นหลังตรงขอบๆปุ่มหาย
+        button_back.setFocusPainted(false);
+        button_back.setOpaque(false);
+        button_back.setBounds(590 ,5 ,95, 20);
+        
+        panel.add(button_back);
+        panel.add(screen);
+       
+        frame.add(panel);
 	    frame.setVisible(true);
+
+	    
+	    button_back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	frame.getContentPane().removeAll();
+            	frame.repaint();
+                lobby = new Lobby(frame);
+            }
+        });
 		
 	}
 	
